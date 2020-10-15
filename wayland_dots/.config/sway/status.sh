6e1a2b1a172185253c95170c5952eb6f9d789111
Silje Raddum <kjempelodott@gmail.com>
2020-10-15 21:01:47 +0200

    add temp

--- wayland_dots/.config/sway/status.sh
+++ wayland_dots/.config/sway/status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#FOCUSED=`swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused==true) | .name'`
-
 TIME=`date +'%H:%M:%S'`
-BATUSE=`acpi -b | awk '{ print $4 }'`
+BATUSE=`acpi -b | awk -F', ' '{ print $2 }'`
+TEMP=`acpi -t | awk '{ print $4 }'`
 COL='\033[0;31m'
 RESET='\033[0m'
 SPACE="<span foreground='coral'>\U00002588\U00002588</span>"
+SMILE="<span foreground='yellow'>\U0001F600</span>"
 
-echo -e "$SPACE" "\U0001F50B" $BATUSE "$SPACE" $TIME
+echo -e "$SPACE" "\U0001F321" $TEMP "$SPACE" "\U0001F50B" $BATUSE "$SPACE" $TIME $SMILE
 
